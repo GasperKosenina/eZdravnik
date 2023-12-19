95724e4a0bea8793ab00efccc54bf706f0ac783b
LukaPrepadnik <120398225+LukaPrepadnik@users.noreply.github.com>
2023-12-19 15:42:45 +0100

    Prvotna struktura

---
+++ frontend/navigation/zasloni/Zgodovina.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Zgodovina() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Zgodovina</Text>
+        </View>
+    );
+}

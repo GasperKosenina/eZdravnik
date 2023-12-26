8a8c225649d15ecc8e44b7cd82d078cf12534604
Gasper <gasper.kosenina@student.um.si>
2023-12-26 13:00:44 +0100

    dodal google-signin

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
-
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
